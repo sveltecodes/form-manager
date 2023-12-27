e8d6d0083ccb28c5b79fe09131bd578bc397cd4a
Matthew Davis <matthew@matthewdavis.io>
2023-12-27 17:59:02 -0500

    fix overlay closing

--- src/lib/form-password-field.svelte
+++ src/lib/form-password-field.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { getContext, onMount } from "svelte";
+	import type { HTMLInputAttributes } from "svelte/elements";
+	import FieldError from "./field-error.svelte";
 	import type { Form } from "./form";
 	import type { FormContextType } from "./form-context";
-	import FieldError from "./field-error.svelte";
-	import type { HTMLInputAttributes } from "svelte/elements";
 
 	export { className as class };
 
@@ -34,6 +34,7 @@
 	bind:value
 	{name}
 	{placeholder}
+	data-1p-ignore
 	class="h-9 rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 {$$props.class}"
 />
 
