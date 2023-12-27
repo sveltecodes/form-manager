8b78bb21cd9d671e5fa85406951911977eb7a809
Matthew Davis <matthew@matthewdavis.io>
2023-12-27 02:21:34 -0500

    maxLength fix

--- src/lib/validators/max-length.ts
+++ src/lib/validators/max-length.ts
@@ -6,8 +6,8 @@ import type { FormValidator } from "../form-validator";
 // };
 
 export const maxLength = (length: number): FormValidator<any> => {
-  return {
-    fn: (value: string) => (value ? value.length >= length : true),
-    message: `must be at most ${length} characters`,
-  };
+	return {
+		fn: (value: string) => (value ? value.length <= length : true),
+		message: `must be at most ${length} characters`
+	};
 };
