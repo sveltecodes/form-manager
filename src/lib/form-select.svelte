c0d2edcec58b033f20e490f6a9b6a467bae4304a
mooy <61348580+mooyg@users.noreply.github.com>
2024-01-03 09:17:57 +0530

    fix: select not closing

--- src/lib/form-select.svelte
+++ src/lib/form-select.svelte
@@ -5,6 +5,7 @@
 	import type { FormContextType } from ".";
 	import { Form } from "./form";
 	import type { SelectOption } from "./types";
+	import { writable } from "svelte/store";
 
 	let selectOptions: SelectOption[];
 	export { className as class };
@@ -59,10 +60,19 @@
 			}
 		}
 	}
+	const open = writable(false);
 </script>
 
-<Select.Root loop bind:selected={bindedValue}>
+<Select.Root
+	loop
+	bind:selected={bindedValue}
+	onSelectedChange={() => {
+		open.set(false);
+	}}
+	open={$open}
+>
 	<Select.Trigger
+		on:click={() => open.set(true)}
 		class="flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
 	>
 		<Select.Value {placeholder} aria-label={placeholder} />
