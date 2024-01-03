156dbc4a027ee92088d26bf5a2e120de57571166
mooy <61348580+mooyg@users.noreply.github.com>
2024-01-03 09:49:14 +0530

    bump

--- src/lib/form-select.svelte
+++ src/lib/form-select.svelte
@@ -62,13 +62,7 @@
 	}
 </script>
 
-<Select.Root
-	loop
-	bind:selected={bindedValue}
-	onOpenChange={() => {
-		console.log("what");
-	}}
->
+<Select.Root loop bind:selected={bindedValue}>
 	<Select.Trigger
 		class="flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
 	>
@@ -77,7 +71,6 @@
 	</Select.Trigger>
 
 	<Select.Content
-		strategy="fixed"
 		class="max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
 	>
 		{#each selectOptions as option}
