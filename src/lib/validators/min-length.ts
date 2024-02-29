6757b5d858cc9d5e7e49efca88e1d408e905eea7
Matthew Davis <matthew@matthewdavis.io>
2024-02-29 17:14:25 -0500

    equal validator

--- src/lib/validators/min-length.ts
+++ src/lib/validators/min-length.ts
@@ -1,10 +1,5 @@
 import type { FormValidator } from "../form-validator";
 
-// export const formMinValidator: FormValidator<any> = {
-// 	fn: (value: string) => (value ? value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)?.length > 0 : false),
-// 	message: "Invalid email address"
-// };
-
 export const minLength = (length: number): FormValidator<any> => {
 	return {
 		fn: (value: string) => (value ? value.length >= length : false),
---
+++ src/lib/validators/must-equal.ts
@@ -0,0 +1,13 @@
+import type { FormValidator } from "../form-validator";
+
+// export const formMinValidator: FormValidator<any> = {
+// 	fn: (value: string) => (value ? value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)?.length > 0 : false),
+// 	message: "Invalid email address"
+// };
+
+export const mustEqaul = (v: any): FormValidator<any> => {
+	return {
+		fn: (value: string) => (value == v),
+		message: `values must match`
+	};
+};
