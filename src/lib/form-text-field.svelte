059ff85a337180ac3772ea00e12d8a5f986d29f5
Matthew Davis <matthew@matthewdavis.io>
2024-02-29 19:29:06 -0500

    default value

--- src/lib/form-text-field.svelte
+++ src/lib/form-text-field.svelte
@@ -36,7 +36,7 @@
 	{placeholder}
 	data-1p-ignore
 	class={twMerge(
-		"h-9 rounded-md border border-gray-600 bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+		"h-9 rounded-md border border-gray-600 bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-gray-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
 		$$props.class
 	)}
 />
